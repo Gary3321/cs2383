1e6fc2cb6ac1ddebcc09dfcf3b7d7389bb442c36
hongchangbao <31748641+hongchangbao@users.noreply.github.com>
2018-03-22 15:44:07 -0300

    Add files via upload

---
+++ readfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main() {   
+
+       static const char filename[] = "test.txt";
+	
+
+       FILE *file = fopen ( filename, "r" );
+       if ( file != NULL )
+       {
+          char line [ 128 ]; /* or other suitable maximum line size */
+          while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
+          {
+             //fputs ( line, stdout ); /* write the line */
+	     printf("%s", line);
+	     char* token = strtok(line, " ");
+	     while(token)
+		{
+		  puts(token);
+		  token=strtok(NULL," ");
+		}  
+
+          }
+          fclose ( file );
+       }
+       else
+       {
+          perror ( filename ); /* why didn't the file open? */
+       }
+       return 0;
+    }
+ 
